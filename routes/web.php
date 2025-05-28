a7e21d8db61ee50737c517549a1f5fed88d5c1e6
Mathias Schreiber <mathias.schreiber@schreiber-tosic.com>
2025-05-28 17:10:06 +0200

    [FEATURE] Added Navigation

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [WelcomeController::class, 'index']);
-Route::get('issuesByMonth', [\App\Http\Controllers\IssuesByMonthController::class, 'index']);
+Route::get('/', [WelcomeController::class, 'index'])->name('home');
+Route::get('issuesByMonth', [\App\Http\Controllers\IssuesByMonthController::class, 'index'])->name('issuesByMonth');
