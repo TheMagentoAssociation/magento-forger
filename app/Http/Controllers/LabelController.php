42f90a8c34b441d463b4d79f11c9253e8e57d5a9
Mathias Schreiber <mathias.schreiber@schreiber-tosic.com>
2025-05-29 21:42:24 +0200

    [FEATURE] Added PRs without Component Label

--- app/Http/Controllers/LabelController.php
+++ app/Http/Controllers/LabelController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use DateTime;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 use OpenSearch\Client;
@@ -57,5 +58,82 @@ class LabelController extends Controller
         }
         ksort($nestedLabels);
         return view('labels/allLabels', ['labels' => $nestedLabels]);
+    }
+
+    public function listPrWithoutComponentLabel(Client $client): view
+    {
+        $params = [
+            'index' => 'github-pull-requests',
+            'body'  => [
+                'size' => 0,
+                'query' => [
+                    'bool' => [
+                        'must' => [
+                            [ 'term' => [ 'is_open' => true ] ]
+                        ],
+                        'must_not' => [
+                            [
+                                'regexp' => [
+                                    'labels.keyword' => 'Component:.*'
+                                ]
+                            ]
+                        ]
+                    ]
+                ],
+                'aggs' => [
+                    'by_year' => [
+                        'date_histogram' => [
+                            'field' => 'created_at',
+                            'calendar_interval' => 'year',
+                            'format' => 'yyyy',
+                            'order' => [ '_key' => 'asc' ],
+                            'min_doc_count' => 1
+                        ],
+                        'aggs' => [
+                            'by_month' => [
+                                'date_histogram' => [
+                                    'field' => 'created_at',
+                                    'calendar_interval' => 'month',
+                                    'format' => 'MM',
+                                    'order' => [ '_key' => 'asc' ],
+                                    'min_doc_count' => 1
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ];
+        $result = $client->search($params);
+        $dataToDisplay = [];
+        foreach ($result['aggregations']['by_year']['buckets'] as $yearBucket) {
+            $dataToDisplay[$yearBucket['key_as_string']] = [
+                'year' => $yearBucket['key_as_string'],
+                'total' => $yearBucket['doc_count'],
+                'months' => [
+                    '01' => ['month_number' => '01', 'total' => 0, 'start' => null, 'end' => null],
+                    '02' => ['month_number' => '02', 'total' => 0, 'start' => null, 'end' => null],
+                    '03' => ['month_number' => '03', 'total' => 0, 'start' => null, 'end' => null],
+                    '04' => ['month_number' => '04', 'total' => 0, 'start' => null, 'end' => null],
+                    '05' => ['month_number' => '05', 'total' => 0, 'start' => null, 'end' => null],
+                    '06' => ['month_number' => '06', 'total' => 0, 'start' => null, 'end' => null],
+                    '07' => ['month_number' => '07', 'total' => 0, 'start' => null, 'end' => null],
+                    '08' => ['month_number' => '08', 'total' => 0, 'start' => null, 'end' => null],
+                    '09' => ['month_number' => '09', 'total' => 0, 'start' => null, 'end' => null],
+                    '10' => ['month_number' => '10', 'total' => 0, 'start' => null, 'end' => null],
+                    '11' => ['month_number' => '11', 'total' => 0, 'start' => null, 'end' => null],
+                    '12' => ['month_number' => '12', 'total' => 0, 'start' => null, 'end' => null],
+                ]
+            ];
+            foreach ($yearBucket['by_month']['buckets'] as $monthBucket) {
+                $dataToDisplay[$yearBucket['key_as_string']]['months'][$monthBucket['key_as_string']]['total'] = $monthBucket['doc_count'];
+                $monthDate = Datetime::createFromFormat('Y-m-d', $yearBucket['key_as_string'] . '-' . $monthBucket['key_as_string'] . '-01');
+                $firstOfMonth = (clone $monthDate)->modify('first day of this month')->setTime(0, 0, 0);
+                $lastOfMonth = (clone $monthDate)->modify('last day of this month')->setTime(23, 59, 59);
+                $dataToDisplay[$yearBucket['key_as_string']]['months'][$monthBucket['key_as_string']]['start'] = $firstOfMonth->format('Y-m-d\TH:i:s\Z');
+                $dataToDisplay[$yearBucket['key_as_string']]['months'][$monthBucket['key_as_string']]['end'] = $lastOfMonth->format('Y-m-d\TH:i:s\Z');
+            }
+        }
+        return view('labels/prsWithoutComponentLabel', ['issues' => $dataToDisplay]);
     }
 }
---
+++ resources/views/labels/prsWithoutComponentLabel.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')
+
+@section('content')
+    @php
+        $currentYear = date('Y');
+        $currentMonth = date('m');
+    @endphp
+    <h2 class="text-3xl font-light mb-6 text-center">PRs without a component label</h2>
+    <div class="container">
+        @foreach($issues as $year)
+            <div class="row">
+                <div class="col-12">
+                    <h3>{{ $year['year']  }} <span class="text-secondary">({{ $year['total'] }} Issues)</span> </h3>
+                </div>
+                @foreach($year['months'] as $month)
+                    @if (!($year['year'] == $currentYear && $month['month_number'] > $currentMonth))
+                        <div class="col-1">
+                            <h5 class="{{ $month['total'] === 0 ? 'text-muted' : '' }}">{{ $month['month_number'] }}</h5>
+                            <p class="{{ $month['total'] === 0 ? 'text-muted' : '' }}">
+                                @if ($month['total'] > 0)
+                                    @php
+                                        $githubUrl = "https://github.com/magento/magento2/pulls?q=is%3Apr+is%3Aopen+-label%3A%22Component%3A%22+created%3A" . $month['start'] . ".." . $month['end'];
+                                    @endphp
+                                    <a href="{{ $githubUrl }}" target="magentoForgerGitHub">
+                                        @endif
+                                        {{ $month['total'] }}&nbsp;Issues
+                                        @if ($month['total'] > 0)
+                                    </a>
+                                @endif
+                            </p>
+                        </div>
+                    @endif
+                @endforeach
+            </div>
+        @endforeach
+    </div>
+@endsection
