cfc497281e720c8e485b11ac377364a67ebd442a
Mathias Schreiber <mathias.schreiber@schreiber-tosic.com>
2025-07-17 17:48:02 +0200

    [BUGFIX] Apply card fix to PRs without component label as well

--- resources/views/labels/prsWithoutComponentLabel.blade.php
+++ resources/views/labels/prsWithoutComponentLabel.blade.php
@@ -14,21 +14,20 @@
                 @foreach($year['months'] as $month)
                     @if (!($year['year'] == $currentYear && $month['month_number'] > $currentMonth))
                         <div class="col-6 col-sm-4 col-md-3 col-lg-2 col-xl-1 mb-3">
-                            <div class="card h-100 text-center calendar-card {{ $month['total'] > 0 ? 'has-link' : '' }}">
+                            <div class="card h-100 text-center calendar-card {{ $month['total'] > 0 ? 'has-link' : '' }} {{ $month['total'] === 0 ? 'opacity-50 pe-none' : '' }}">
                                 <div class="card-body p-2">
-                                    <h6 class="card-title {{ $month['total'] === 0 ? 'text-muted' : '' }}">{{ $month['month_number'] }}</h6>
-                                    <p class="card-text small {{ $month['total'] === 0 ? 'text-muted' : '' }}" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">
+                                    <h6 class="card-title {{$month['total'] > 0 ? 'text-primary' : 'text-muted'}}">{{ $month['month_number'] }}</h6>
+                                    <p class="card-text small mb-0 {{$month['total'] > 0 ? 'text-primary' : 'text-muted'}}" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">
+                                        {{ $month['total'] }}&nbsp;PRs
+                                    </p>
                                         @if ($month['total'] > 0)
                                             @php
                                                 $githubUrl = "https://github.com/magento/magento2/pulls?q=is%3Apr+is%3Aopen+-label%3A%22Component%3A%22+created%3A" . $month['start'] . ".." . $month['end'];
                                             @endphp
-                                            <a href="{{ $githubUrl }}" target="magentoForgerGitHub" class="text-decoration-none">
+                                        <a href="{{ $githubUrl }}" target="magentoForgerGitHub" class="stretched-link">
+                                            <span class="visually-hidden">View Issues</span>
+                                        </a>
                                         @endif
-                                        {{ $month['total'] }}&nbsp;PRs
-                                        @if ($month['total'] > 0)
-                                            </a>
-                                        @endif
-                                    </p>
                                 </div>
                             </div>
                         </div>
