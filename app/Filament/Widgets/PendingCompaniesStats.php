07487c64919c07c22e0dda0ed5bfaba9de308ab2
Francis Gallagher <francis917@gmail.com>
2026-01-10 00:17:33 +0000

    chore(pr-feedback) improve pending company widget

--- app/Filament/Widgets/PendingCompaniesStats.php
+++ app/Filament/Widgets/PendingCompaniesStats.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Widgets;
 
+use App\Filament\Resources\CompanyResource;
 use App\Models\Company;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
@@ -21,7 +22,9 @@ class PendingCompaniesStats extends BaseWidget
                 ->description('Companies awaiting review')
                 ->descriptionIcon('heroicon-m-clock')
                 ->color($pendingCount > 0 ? 'warning' : 'success')
-                ->url('/admin/companies?tableFilters[status][value]=pending'),
+                ->url(CompanyResource::getUrl('index', [
+                    'tableFilters' => ['status' => ['value' => 'pending']]
+                ])),
 
             Stat::make('Approved Companies', $approvedCount)
                 ->description('Active companies')
